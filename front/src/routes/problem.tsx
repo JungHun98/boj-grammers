ab7c0c5fcd0ec6bf2fd409b87cfe76d5f6b3b918
JungHun98 <j7papa@naver.com>
2024-11-18 15:02:04 +0900

    chore: 컴포넌트 분리

--- front/src/routes/problem.tsx
+++ front/src/routes/problem.tsx
@@ -1,46 +1,27 @@
-import { useState, useCallback, useEffect, useRef, DragEventHandler, ChangeEventHandler } from "react";
-import { useParams, useBlocker, useNavigate } from "react-router-dom";
-import CodeMirror from "@uiw/react-codemirror";
-import { copilot } from "@uiw/codemirror-theme-copilot";
-import io from "socket.io-client";
-import { LanguageName, loadLanguage } from "@uiw/codemirror-extensions-langs";
+import {
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+  DragEventHandler,
+} from 'react';
+import { useParams, useBlocker, useNavigate } from 'react-router-dom';
 
-import { IOutput, IProblem } from "../types";
-import { defaultCode } from "../utils/consts";
+import CodeMirror from '@uiw/react-codemirror';
+import { copilot } from '@uiw/codemirror-theme-copilot';
+import io from 'socket.io-client';
+import { LanguageName, loadLanguage } from '@uiw/codemirror-extensions-langs';
 
-import styled from "@emotion/styled";
+import { IOutput, IProblem } from '../types';
+import { defaultCode } from '../utils/consts';
+
+import ProblemSearch from '../components/ProblemSearch';
+import ProblemContainer from '../components/ProblemContainer';
 
 interface IDialog {
   title: string;
   text: string;
 }
-
-interface ProblemSectionProps {
-  title: string;
-  html: string;
-}
-
-interface ExampleProps {
-  content: string;
-}
-
-function ProblemSection({title, html}: ProblemSectionProps) {
-  return( 
-  <div>
-    <ProblemH6>{title}</ProblemH6>
-    <div dangerouslySetInnerHTML={{ __html: html}}></div>
-  </div>
-  )
-}
-
-function Example({content}: ExampleProps) {
-  return( 
-  <div>
-    <pre>{content}</pre>
-  </div>
-  )
-}
-
 
 const Problem = () => {
   const navigate = useNavigate();
@@ -51,52 +32,28 @@ const Problem = () => {
 
   const [leftWidth, setLeftWidth] = useState(40);
   const [upHeigth, setUpHeigth] = useState(40);
-  
-  const [lang, setLang] = useState<LanguageName>("javascript");
+
+  const [lang, setLang] = useState<LanguageName>('javascript');
   const [result, setResult] = useState<IOutput[] | null>();
   const [output, setOutput] = useState<IOutput[] | null>();
   const [error, setError] = useState<string | null>();
   const [dialogText, setDialogText] = useState<IDialog | null>();
 
-  const [problem, setProblem] = useState({title:'', descriptionHtml:'', inputHtml: '',
-    outputHtml: '',
-    limitHtml: '',
-    examples: [{
-      explain: '',
-      input: '',
-      number: 0,
-      output: '' 
-    }]});
-  const [problemNumber, setProblemNumber] = useState(1000);
-
-  async function ft () {
-    const result = await fetch(`http://localhost:8080/api/problem?problemId=${problemNumber}`).then((response) => response.json());
-    setProblem(result);
-  }
-  
-
-  const handleDrag: DragEventHandler= (e) => {
+  const handleDrag: DragEventHandler = (e) => {
     const newLeftWidth = (e.clientX / window.innerWidth) * 100;
-    if (newLeftWidth > 5 && newLeftWidth < 95) { // 최소, 최대 너비 설정
+    if (newLeftWidth > 5 && newLeftWidth < 95) {
+      // 최소, 최대 너비 설정
       setLeftWidth(newLeftWidth);
     }
   };
 
-  const handleVirticalDrag: DragEventHandler= (e) => {
+  const handleVirticalDrag: DragEventHandler = (e) => {
     const newUpHegith = (e.clientY / window.innerHeight) * 100;
-    if (newUpHegith > 5 && newUpHegith < 95) { // 최소, 최대 너비 설정
+    if (newUpHegith > 5 && newUpHegith < 95) {
+      // 최소, 최대 너비 설정
       setUpHeigth(newUpHegith);
     }
   };
-
-  const handleProblemInput: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const inputElement = e.target;
-    setProblemNumber(Number(inputElement.value));
-  }
-
-  const handleSearchProblem = () => {
-    ft();
-  }
 
   // const onSubmit = useCallback(() => {
   //   setResult(null);
@@ -127,16 +84,16 @@ const Problem = () => {
     setOutput(null);
     setError(null);
     switch (lang) {
-      case "javascript":
+      case 'javascript':
         codeRef.current = defaultCode.javscript;
         break;
-      case "python":
+      case 'python':
         codeRef.current = defaultCode.python;
         break;
-      case "java":
+      case 'java':
         codeRef.current = defaultCode.java;
         break;
-      case "cpp":
+      case 'cpp':
         codeRef.current = defaultCode.cpp;
         break;
       default:
@@ -145,31 +102,31 @@ const Problem = () => {
   }, [lang]);
 
   // useEffect(() => {
-    // socket.on("test", (data) => {
-    //   setResult(data);
-    //   if (data.every((v: IOutput) => v.output !== null)) {
-    //     setTimeout(() => {
-    //       data.some((v: IOutput) =>
-    //         !v.output
-    //           ? setDialogText({
-    //               title: "실패",
-    //               text: "아쉽게도 테스트에 통과하지 못 했습니다.",
-    //             })
-    //           : setDialogText({
-    //               title: "성공",
-    //               text: "모든 테스트를 통과헸습니다. 축하드립니다!",
-    //             })
-    //       );
-    //       dialogRef.current?.showModal();
-    //     }, 500);
-    //   }
-    // });
-    // socket.on("output", (data) => {
-    //   setOutput(data);
-    // });
-    // socket.on("error", (data) => {
-    //   setError(data);
-    // });
+  // socket.on("test", (data) => {
+  //   setResult(data);
+  //   if (data.every((v: IOutput) => v.output !== null)) {
+  //     setTimeout(() => {
+  //       data.some((v: IOutput) =>
+  //         !v.output
+  //           ? setDialogText({
+  //               title: "실패",
+  //               text: "아쉽게도 테스트에 통과하지 못 했습니다.",
+  //             })
+  //           : setDialogText({
+  //               title: "성공",
+  //               text: "모든 테스트를 통과헸습니다. 축하드립니다!",
+  //             })
+  //       );
+  //       dialogRef.current?.showModal();
+  //     }, 500);
+  //   }
+  // });
+  // socket.on("output", (data) => {
+  //   setOutput(data);
+  // });
+  // socket.on("error", (data) => {
+  //   setError(data);
+  // });
   // }, [socket]);
 
   // useEffect(() => {
@@ -184,12 +141,12 @@ const Problem = () => {
   // }, [socket]);
 
   useEffect(() => {
-    if (blocker.state === "blocked") {
-      const isTrue = window.confirm("변경사항이 저장되지 않을 수 있습니다.");
+    if (blocker.state === 'blocked') {
+      const isTrue = window.confirm('변경사항이 저장되지 않을 수 있습니다.');
 
       if (isTrue) {
         blocker.proceed();
-        navigate("/");
+        navigate('/');
       } else {
         blocker.reset();
       }
@@ -200,12 +157,7 @@ const Problem = () => {
     <>
       <div className="wrapper">
         <div className="header">
-          <div className="title">
-            <ProblemNumberInput onChange={handleProblemInput}/>
-            <ProblemSearchButton onClick={handleSearchProblem}>
-              <img src='/search.svg' alt='' width={24}/>
-            </ProblemSearchButton>
-          </div>
+          <ProblemSearch />
           <div className="select-lang">
             <select
               onChange={(e) => setLang(e.target.value as LanguageName)}
@@ -219,30 +171,17 @@ const Problem = () => {
           </div>
         </div>
         <main>
-          <div className="problem" style={{ width: `${leftWidth}%` }}>  
-            <div>
-              <h3>{problem.title}</h3>
-            </div>
-            <ProblemSection title="문제설명" html={problem.descriptionHtml}/>
-            <ProblemSection title="입력" html={problem.inputHtml}/>
-            <ProblemSection title="출력" html={problem.outputHtml}/>
-            {problem.limitHtml !== null ? <ProblemSection title="제한" html={problem.limitHtml}/> : null}
-
-            {problem.examples!== null ? problem.examples.map(({explain, input, number, output}) => {
-              return (
-              <div key={number}>
-                <ProblemH6>예제 입력 {number}</ProblemH6>
-                <Example content={input}/>
-                <ProblemH6>예제 출력 {number}</ProblemH6>
-                <Example content={output}/>
-              </div>
-              )
-            }) : null}
-          </div>
+          <ProblemContainer width={leftWidth} />
           <div className="gutter" draggable="true" onDrag={handleDrag}></div>
-          <div className="code-wrapper" style={{ width: `${100 - leftWidth}%` }}>
+          <div
+            className="code-wrapper"
+            style={{ width: `${100 - leftWidth}%` }}
+          >
             <div className="solution-wrapper">
-              <div className="codeMirror-wrapper" style={{ height: `${upHeigth}%` }}>
+              <div
+                className="codeMirror-wrapper"
+                style={{ height: `${upHeigth}%` }}
+              >
                 <CodeMirror
                   value={codeRef.current}
                   onChange={(e) => {
@@ -251,18 +190,22 @@ const Problem = () => {
                   theme={copilot}
                   height="100%"
                   extensions={[loadLanguage(`${lang}`)!]}
-                  />
+                />
               </div>
 
-                <div className="gutter_vertical" draggable="true" onDrag={handleVirticalDrag}></div>
+              <div
+                className="gutter_vertical"
+                draggable="true"
+                onDrag={handleVirticalDrag}
+              ></div>
 
               <div className="output" style={{ height: `${100 - upHeigth}%` }}>
                 {error ? (
                   <p
                     style={{
-                      margin: "0px",
-                      color: "#c92c2c",
-                      whiteSpace: "pre-wrap",
+                      margin: '0px',
+                      color: '#c92c2c',
+                      whiteSpace: 'pre-wrap',
                     }}
                   >
                     {error}
@@ -273,16 +216,18 @@ const Problem = () => {
                       <>
                         <h3>실행 결과</h3>
                         {output.map((v, i) => {
-                          const input = v.input?.includes("\\n")
-                            ? v.input.split("\\n").join("\n")
+                          const input = v.input?.includes('\\n')
+                            ? v.input.split('\\n').join('\n')
                             : v.input;
                           const output =
-                            typeof v.output === "string" && v.output.includes("\\n")
-                              ? v.output.split("\\n").join("\n")
+                            typeof v.output === 'string' &&
+                            v.output.includes('\\n')
+                              ? v.output.split('\\n').join('\n')
                               : v.output;
                           const result =
-                            typeof v.result === "string" && v.result.includes("\\n")
-                              ? v.result.split("\\n").join("\n")
+                            typeof v.result === 'string' &&
+                            v.result.includes('\\n')
+                              ? v.result.split('\\n').join('\n')
                               : v.result;
 
                           return (
@@ -290,20 +235,20 @@ const Problem = () => {
                               <tbody>
                                 <tr>
                                   <th>입력값</th>
-                                  <td style={{ whiteSpace: "pre-line" }}>
-                                    {input ?? "로딩중..."}
+                                  <td style={{ whiteSpace: 'pre-line' }}>
+                                    {input ?? '로딩중...'}
                                   </td>
                                 </tr>
                                 <tr>
                                   <th>기댓값</th>
-                                  <td style={{ whiteSpace: "pre-line" }}>
-                                    {output ?? "로딩중..."}
+                                  <td style={{ whiteSpace: 'pre-line' }}>
+                                    {output ?? '로딩중...'}
                                   </td>
                                 </tr>
                                 <tr>
                                   <th>출력</th>
-                                  <td style={{ whiteSpace: "pre-line" }}>
-                                    {result ?? "로딩중..."}
+                                  <td style={{ whiteSpace: 'pre-line' }}>
+                                    {result ?? '로딩중...'}
                                   </td>
                                 </tr>
                               </tbody>
@@ -315,9 +260,7 @@ const Problem = () => {
                     {result && (
                       <>
                         <h3>체점 결과</h3>
-                        <ul className="result-list">
-                          
-                        </ul>
+                        <ul className="result-list"></ul>
                       </>
                     )}
                   </>
@@ -333,7 +276,6 @@ const Problem = () => {
               </button> */}
             </div>
           </div>
-        
         </main>
       </div>
       <dialog ref={dialogRef}>
@@ -352,38 +294,3 @@ const Problem = () => {
 };
 
 export default Problem;
-
-const ProblemNumberInput = styled('input')`
-  padding: 0px 1.5rem 0px 1.0625rem;
-  width: 10rem;
-  height: 1.7rem;
-  background-color: #151f29;
-  border: 0.05rem solid #d5d5d5;
-  border-radius: 0.25rem;
-  appearance: none;
-  color: white;
-  caret-color: white;
-
-  &:hover, &:focus-within {
-    outline: none;
-    border: 0.05rem solid rgb(0, 120, 255);
-    box-shadow: rgb(0, 120, 255) 0px 0
-  }
-`
-
-const ProblemSearchButton = styled('button')`
-  position: absolute;
-  right: 0;
-  top: 2px;
-  width: 24px;
-  height: 24px;
-  background-color: transparent;
-  cursor: pointer;
-`
-
-const ProblemH6 = styled('h6')`
-font-weight: 700;
-font-size: 14px;
-    margin-bottom: 1rem;
-    color: white;
-`
