643c7ebb276b8e2f6936d5947ae76f9092146b77
JungHun98 <j7papa@naver.com>
2024-11-20 15:57:34 +0900

    feat: store 예제 입출력 데이터 추가

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -2,17 +2,27 @@ import { create } from 'zustand';
 
 type State = {
   problemNumber: number;
+  exampleInput: string[];
+  exampleOutput: string[];
 };
 
 type Action = {
   updateProblemNumber: (problemNumber: State['problemNumber']) => void;
+  updateExampleInput: (exampleInput: State['exampleInput']) => void;
+  updateExampleOutput: (exampleOutput: State['exampleOutput']) => void;
 };
 
 // Create your store, which includes both state and (optionally) actions
 const useProblemStore = create<State & Action>((set) => ({
   problemNumber: 1000,
+  exampleInput: [],
+  exampleOutput: [],
   updateProblemNumber: (problemNumber) =>
     set(() => ({ problemNumber: problemNumber })),
+  updateExampleInput: (exampleInput) =>
+    set(() => ({ exampleInput: exampleInput })),
+  updateExampleOutput: (exampleOutput) =>
+    set(() => ({ exampleOutput: exampleOutput })),
 }));
 
 export default useProblemStore;
