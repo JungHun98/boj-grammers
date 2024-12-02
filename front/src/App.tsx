233dd463bff7ebe750f2a155b0ebd194a1fec0be
JungHun98 <j7papa@naver.com>
2024-12-02 17:45:42 +0900

    feat: Modal 활성화 여부 상태 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,24 @@
-import "./Main.css";
-import Problem from "./routes/problem";
+import './Main.css';
+import Problem from './routes/problem';
+import Modal from './components/common/Modal/Modal';
+import { useState } from 'react';
+import TestCaseModal from './components/TestCaseModal';
 
 function App() {
-  return <Problem/>;
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleModalClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <Problem />
+      <Modal open={isOpen} onClose={handleModalClose}>
+        <TestCaseModal onClose={handleModalClose} />
+      </Modal>
+    </>
+  );
 }
 
 export default App;
