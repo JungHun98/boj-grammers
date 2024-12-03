76a875310b661915fc36d746af2af03a9734ee1e
JungHun98 <j7papa@naver.com>
2024-12-03 17:59:19 +0900

    feat: 테스트케이스 추가 모달 상태 구현

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,20 +3,44 @@ import Problem from './routes/problem';
 import Modal from './components/common/Modal/Modal';
 import { useState } from 'react';
 import TestCaseModal from './components/TestCaseModal';
+import AdditionalTestCaseModal from './components/AdditionalTestCaseModal';
+import AdditionalModal from './components/common/AdditionalModal/AdditionalModal';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isAdditionalOpen, setAdditionalOpen] = useState(false);
 
   const handleModalClose = () => {
     setIsOpen(false);
   };
 
+  const handleModalOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleAdditionalModalClose = () => {
+    setAdditionalOpen(false);
+  };
+
+  const handleAdditionalModalOpen = () => {
+    setAdditionalOpen(true);
+  };
+
   return (
     <>
-      <Problem />
+      <Problem onOpen={handleModalOpen} />
       <Modal open={isOpen} onClose={handleModalClose}>
-        <TestCaseModal onClose={handleModalClose} />
+        <TestCaseModal
+          onClose={handleModalClose}
+          onOpen={handleAdditionalModalOpen}
+        />
       </Modal>
+      <AdditionalModal
+        open={isAdditionalOpen}
+        onClose={handleAdditionalModalClose}
+      >
+        <AdditionalTestCaseModal onClose={handleAdditionalModalClose} />
+      </AdditionalModal>
     </>
   );
 }
