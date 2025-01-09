65b969272d0d9fbbb7c38fb792563527929a010e
JungHun98 <j7papa@naver.com>
2025-01-09 18:21:04 +0900

    feat: 코드 변경 사항 저장 기능 구현

--- front/src/components/CodeSpace/CodeSpace.tsx
+++ front/src/components/CodeSpace/CodeSpace.tsx
@@ -1,8 +1,9 @@
-import CodeMirror from '@uiw/react-codemirror';
+import CodeMirror, { ViewUpdate } from '@uiw/react-codemirror';
 import { copilot } from '@uiw/codemirror-theme-copilot';
 import { loadLanguage } from '@uiw/codemirror-extensions-langs';
 import { Wrapper } from './CodeSpace.style';
 import useCodeStore from '../../store/codeStroe';
+import { ChangeEventHandler } from 'react';
 
 interface CodeSpaceProps {
   height: number;
@@ -11,12 +12,17 @@ interface CodeSpaceProps {
 function CodeSpace({ height }: CodeSpaceProps) {
   const lang = useCodeStore((state) => state.language);
   const codeSet = useCodeStore((state) => state.code);
+  const updateCode = useCodeStore((state) => state.updateCode);
+
+  const handleInputCode = (value: string, viewUpdate: ViewUpdate) => {
+    updateCode(lang, value);
+  };
 
   return (
     <Wrapper height={height}>
       <CodeMirror
         value={codeSet[lang]}
-        onChange={(e) => {}}
+        onChange={handleInputCode}
         theme={copilot}
         height="100%"
         extensions={[loadLanguage(lang)!]}
