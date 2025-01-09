ec02140daae302f36bc9e6fc4728a657010a0725
JungHun98 <j7papa@naver.com>
2025-01-09 15:55:33 +0900

    feat: store 풀이 언어 상태 추가

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 
 type State = {
   problemNumber: number;
+  language: string;
   exampleInput: string[];
   exampleOutput: string[];
 };
@@ -10,11 +11,13 @@ type Action = {
   updateProblemNumber: (problemNumber: State['problemNumber']) => void;
   updateExampleInput: (exampleInput: State['exampleInput']) => void;
   updateExampleOutput: (exampleOutput: State['exampleOutput']) => void;
+  updateLanguage: (language: State['language']) => void;
 };
 
 // Create your store, which includes both state and (optionally) actions
 const useProblemStore = create<State & Action>((set) => ({
   problemNumber: 1000,
+  language: 'java',
   exampleInput: [],
   exampleOutput: [],
   updateProblemNumber: (problemNumber) =>
@@ -23,6 +26,7 @@ const useProblemStore = create<State & Action>((set) => ({
     set(() => ({ exampleInput: exampleInput })),
   updateExampleOutput: (exampleOutput) =>
     set(() => ({ exampleOutput: exampleOutput })),
+  updateLanguage: (language) => set(() => ({ language: language })),
 }));
 
 export default useProblemStore;
