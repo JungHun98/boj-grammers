790fedd2adf23cf9640b14d3ef6ee8982613d28e
JungHun98 <j7papa@naver.com>
2025-01-14 18:45:46 +0900

    feat: fetch 커스텀 훅 수정

--- front/src/hooks/useFetchProblem.ts
+++ front/src/hooks/useFetchProblem.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 interface ExampleInputOutput {
   explain: string;
@@ -16,14 +16,20 @@ interface ProblemContent {
   examples: ExampleInputOutput[];
 }
 
-function useFetchProblem(pId: number) : {data: ProblemContent | null, error: Error | null} {
+function useFetchProblem(pId: number): {
+  data: ProblemContent | null;
+  error: Error | null;
+  setData: React.Dispatch<React.SetStateAction<ProblemContent | null>>;
+} {
   const [data, setData] = useState<ProblemContent | null>(null);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch(`http://localhost:8080/api/problem?problemId=${pId}`);
+        const response = await fetch(
+          `http://localhost:8080/api/problem?problemId=${pId}`,
+        );
 
         if (!response.ok) {
           throw new Error('Failed to fetch data');
@@ -32,7 +38,6 @@ function useFetchProblem(pId: number) : {data: ProblemContent | null, error: Err
         const data: ProblemContent = await response.json();
         setData(data);
       } catch (err: unknown) {
-        
         if (err instanceof Error) {
           setError(err);
         } else {
@@ -44,7 +49,7 @@ function useFetchProblem(pId: number) : {data: ProblemContent | null, error: Err
     fetchData();
   }, [pId]);
 
-  return { data, error };
+  return { data, error, setData };
 }
 
 export default useFetchProblem;
