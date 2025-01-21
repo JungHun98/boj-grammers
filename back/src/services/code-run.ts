1b276d0c808b9c5cdca23d454f27a75a830fae6f
JungHun98 <j7papa@naver.com>
2025-01-21 19:40:13 +0900

    feat: 소켓 id 기반 리소스 분리

--- back/src/services/code-run.ts
+++ back/src/services/code-run.ts
@@ -5,11 +5,12 @@ import { TestData } from "./problem-socket";
 import { cleanDirectory } from "../helper/clean-directory";
 import { execSync } from "child_process";
 import { fileName, filePath } from "../consts";
+import { Server } from "socket.io";
 
 const executeCommand = (param: string, compileCommand: string | null, runCommand: string, callback: (err: string, res: any) => void) => {
   const formattedParam = param.split('\n').join('\\n');
 
-  if (compileCommand) {
+  if (compileCommand !== null) {
     execSync(`docker exec test-app sh -c "${compileCommand}"`);
   }
 
@@ -18,41 +19,42 @@ const executeCommand = (param: string, compileCommand: string | null, runCommand
   dockerRun(command, callback);
 }
 
-export const codeRun = (socket: any, data: TestData) => {
+export const codeRun = (socket: any, data: TestData, io: Server) => {
   const { code, lang, input } = data;
 
   const clientResult: string[] = Array(input.length).fill(null);
+  const dockerPath = `/usr/src/${socket.id}`;
 
-  fs.writeFileSync(`${filePath}/${fileName[lang]}`, code);
+  fs.writeFileSync(`${filePath}/${socket.id}/${fileName[lang]}`, code);
 
   try {
-    execSync(`docker cp compile/. test-app:/usr/src`);
+    execSync(`docker cp ${filePath}/${socket.id} test-app:/usr/src`);
   } catch (err) {
     console.error("에러");
-    socket.emit("error", "compile");
+    io.to(socket.id).emit("error", "compile");
     return;
   }
 
   const languageCommands = {
     "javascript": {
       compile: null,
-      run: "node code.js"
+      run: `node ${dockerPath}/code.js`
     },
     "python": {
       compile: null,
-      run: "python3 code.py"
+      run: `python3 ${dockerPath}/code.py`
     },
     "java": {
-      compile: "javac Main.java",
-      run: "java Main"
+      compile: `javac ${dockerPath}/Main.java`,
+      run: `java ${dockerPath}/Main.java`
     },
     "cpp": {
-      compile: "g++ -o main main.cpp",
-      run: "./main"
+      compile: `g++ -o ${dockerPath}/main ${dockerPath}/main.cpp`,
+      run: `${dockerPath}/main.cpp`
     }
   };
 
-  socket.emit("start", clientResult);
+  io.to(socket.id).emit("start", clientResult);
 
   input.forEach((test, i) => {
     const { compile, run } = languageCommands[lang] || {};
@@ -62,18 +64,19 @@ export const codeRun = (socket: any, data: TestData) => {
     try {
       executeCommand(test, compile, run, (err: string, res: any) => {
         if (err) {
-          socket.emit("error", err);
+          io.to(socket.id).emit("error", err);
           return;
         }
         
         const result = typeof res === "object" ? JSON.parse(res) : res;
         clientResult[i] = result;
-        socket.emit("output", clientResult);
+        
+        io.to(socket.id).emit("output", clientResult);
       });
     } catch(err: any) {
-      socket.emit("error", err.message);
+      io.to(socket.id).emit("error", err.message);
     }
   });
 
-  cleanDirectory(filePath);
+  cleanDirectory(`${filePath}/${socket.id}`);
 };
