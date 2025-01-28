29857eadb7c60a3ed7b48a7021d55fedaebae619
JungHun98 <j7papa@naver.com>
2025-01-28 15:08:06 +0900

    feat: 질문 게시판 버튼 추가

--- front/src/components/ButtonSection/ButtonSection.tsx
+++ front/src/components/ButtonSection/ButtonSection.tsx
@@ -18,12 +18,14 @@ function ButtonSection({ onOpen }: ButtonSectionProps) {
 
   const googleSearchURL = `https://www.google.com/search?q=백준+${problemNumber}+${language}`;
   const BojSubmithURL = `https://www.acmicpc.net/submit/${problemNumber}`;
+  const questionURL = `https://www.acmicpc.net/board/search/all/problem/${problemNumber}`;
 
   return (
     <Wrapper>
       <ButtonContainer>
         <Button onClick={onOpen}>테스트 케이스 추가하기</Button>
-        <Button onClick={() => openNewTap(googleSearchURL)}>문제 검색</Button>
+        <Button onClick={() => openNewTap(googleSearchURL)}>문제검색</Button>
+        <Button onClick={() => openNewTap(questionURL)}>질문 게시판</Button>
       </ButtonContainer>
       <ButtonContainer>
         <CodeRunButton />
