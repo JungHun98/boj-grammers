b50dc2c5942daa3c6951d99b9a52d133101057e9
JungHun98 <j7papa@naver.com>
2025-02-02 16:42:39 +0900

    fix: 파일 삭제 예외처리 구현

--- back/src/helper/clean-directory.ts
+++ back/src/helper/clean-directory.ts
@@ -1,9 +1,13 @@
 import fs from "fs";
 
 export const cleanDirectory = (path: string) => {
-  fs.promises.readdir(path).then((dir) => {
-    dir.forEach((file) => {
-      fs.promises.unlink(`${path}/${file}`);
+  try {
+    fs.promises.readdir(path).then((dir) => {
+      dir.forEach((file) => {
+        fs.promises.unlink(`${path}/${file}`);
+      });
     });
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
