c0e4ab791ba7368898225f69727b753bae2137ee
JungHun98 <j7papa@naver.com>
2025-02-05 22:21:13 +0900

    refactor: docker cpu 사용량 조절

--- back/src/helper/docker-build.ts
+++ back/src/helper/docker-build.ts
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 export const dockerBuild = () => {
   try {
     execSync("docker build -t test-image ./docker");
-    execSync("docker run -d --name test-app test-image");
+    execSync('docker run -d --name test-app --cpus="0.8" test-image');
   } catch (error) {
     console.error(
       `Error occurred while building Docker image: ${(error as any).message}`
