9e8dc7717cac3e920aee8244a04ee63c1e516d1e
JungHun98 <j7papa@naver.com>
2025-02-11 10:39:12 +0900

    fix: docker 메모리 사용량 제한

--- back/src/helper/docker-build.ts
+++ back/src/helper/docker-build.ts
@@ -3,7 +3,9 @@ import { execSync } from "child_process";
 export const dockerBuild = () => {
   try {
     execSync("docker build -t test-image ./docker");
-    execSync('docker run -d --name test-app --cpus="0.8" test-image');
+    execSync(
+      'docker run -d --name test-app --cpus="0.8" --memory="1024m" test-image'
+    );
   } catch (error) {
     console.error(
       `Error occurred while building Docker image: ${(error as any).message}`
