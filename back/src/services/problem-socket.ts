b192391a99495f7791265021824d87923c19af81
JungHun98 <j7papa@naver.com>
2025-02-12 20:37:50 +0900

    refactor: 불필요한 도커 명령 제거

--- back/src/services/problem-socket.ts
+++ back/src/services/problem-socket.ts
@@ -86,7 +86,6 @@ export const problemSocket = (io: Server) => {
 
     socket.on("disconnect", () => {
       try {
-        execSync(`docker exec test-app sh -c "rm -rf /usr/src/${socket.id}"`);
         fs.rmdirSync(`compile/${socket.id}`);
       } catch {
         console.log(`fail remove /usr/src/${socket.id}`);
